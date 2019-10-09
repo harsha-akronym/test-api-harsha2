a68ec7de386d1a7f8c6aeb6bf699b0c6b65b6390
h-hub <harsha.kj89@gmail.com>
2019-10-09 14:49:28 +0800

    first commit

---
+++ src/main/java/com/akronym/testApi/TestApiApplication.java
@@ -0,0 +1,23 @@
+package com.akronym.testApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApiApplication.class, args);
+	}
+	
+	@RequestMapping("/status")
+    public String healthCheck() {
+        return "Akronym Secure API is up and running";
+    }
+
+}
