8463ccefa92ec3f5de3fb76cb47f5677f07e78c6
harsha-akronym <harsha@akronym.com>
2019-10-12 00:53:37 +0800

    enable https

--- src/main/java/com/akronym/testApi/TestApiApplication.java
+++ src/main/java/com/akronym/testApi/TestApiApplication.java
@@ -2,12 +2,13 @@ package com.akronym.testApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @RestController
 public class TestApiApplication {
 
---
+++ src/main/java/com/akronym/testApi/config/AppSecurity.java
@@ -0,0 +1,22 @@
+package com.akronym.testApi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class AppSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .requiresChannel().anyRequest().requiresSecure()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/*")
+                .permitAll();
+    }
+}
